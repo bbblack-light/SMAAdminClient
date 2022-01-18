57deee5b8e2af503f6eeca51585b0ff218d22209
bbblack-light <barakova.2000@mail.ru>
2022-01-18 13:01:17 +0400

    login page added

--- src/views/app/app-routing.module.ts
+++ src/views/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './../login-page/login-page.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'first-component',
+    component: AppComponent,
+  },
+  { path: 'login', component: LoginPageComponent },
+];
+todo: check why routes not working
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoutes(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/views/app/app.module.ts
+++ src/views/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoginPageComponent } from './../login-page/login-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LoginPageComponent],
 })
-export class AppModule { }
+export class AppModule {}
