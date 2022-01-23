31d59bc0bd2f9e2a1f18ed5b4495157faa60473c
BarakovaES <barakova.2000@mail.ru>
2022-01-23 20:18:12 +0400

    login page layout finished

--- src/views/login-page/login-page.component.ts
+++ src/views/login-page/login-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  hide = true;
   constructor() {}
 
   ngOnInit() {}
